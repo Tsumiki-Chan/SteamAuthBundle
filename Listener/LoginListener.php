54cbb588feba2d0d7d5dbe416e320b4750d7a6e7
Jonathan K <koenig-jonathan@web.de>
2017-10-16 01:05:12 +0200

    Removed debug logging and fixed a typo

--- Listener/LoginListener.php
+++ Listener/LoginListener.php
@@ -3,7 +3,6 @@
 namespace SteamAuthBundle\Listener;
 
 use Doctrine\ORM\EntityManager;
-use Monolog\Logger;
 use SteamAuthBundle\Service\SteamUserService;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\Security\Core\Event\AuthenticationEvent;
@@ -11,25 +10,18 @@ use Symfony\Component\Security\Core\Event\AuthenticationFailureEvent;
 
 class LoginListener 
 {
-
-    private $logger;
     private $userService;
     private $em;
 
-    public function __construct(Logger $logger, EntityManager $em, SteamUserService $userService) {
-        $this->logger = $logger;
+    public function __construct(EntityManager $em, SteamUserService $userService) {
         $this->userService = $userService;
         $this->em = $em;
-
-        $this->logger->info("LoginListener! __construct");
     }
 
     public function onSecurityInteractivelogin(InteractiveLoginEvent $event) {
-        $this->logger->info("LoginListener! InteractiveLoginEvent");
-
         $user = $event->getAuthenticationToken()->getUser();
 
-        $userService->updateUserEntry($user);
+        $this->userService->updateUserEntry($user);
         
         $this->em->persist($user);
         $this->em->flush($user);
