b07e9743e5f88b496b2f41ce8248da539f1224f1
Northdegree <mirkoiz@hotmail.de>
2017-10-15 22:59:13 +0200

    Fixed missing logger argument

--- Listener/LoginListener.php
+++ Listener/LoginListener.php
@@ -3,13 +3,15 @@
 namespace SteamAuthBundle\Listener;
 
 use Doctrine\ORM\EntityManager;
+use Monolog\Logger;
 use SteamAuthBundle\Service\SteamUserService;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class LoginListener 
 {
 
-    public function __construct(EntityManager $em, SteamUserService $userService) {
+    public function __construct(Logger $logger, EntityManager $em, SteamUserService $userService) {
+        $this->logger = $logger;
         $this->userService = $userService;
         $this->em = $em;
     }
